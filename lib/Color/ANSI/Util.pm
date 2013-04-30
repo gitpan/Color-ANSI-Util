cf4098b08cea91fed13d1a26a8a12b8fc5bfd1fc
Steven Haryanto <stevenharyanto@gmail.com>
2013-04-30 16:46:56 +0000

    Import of SHARYANTO/Color-ANSI-Util-0.03 from CPAN.
    gitpan-cpan-distribution: Color-ANSI-Util
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Color-ANSI-Util-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Color/ANSI/Util.pm
+++ lib/Color/ANSI/Util.pm
@@ -25,9 +25,16 @@ our @EXPORT_OK = qw(
                        ansi24bfg
                        rgb_to_ansi24b_bg_code
                        ansi24bbg
+
+                       rgb_to_ansi_fg_code
+                       ansifg
+                       rgb_to_ansi_bg_code
+                       ansibg
+
+                       detect_color_depth
                );
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 my %ansi16 = (
     0  => '000000',
@@ -241,6 +248,58 @@ sub rgb_to_ansi24b_bg_code {
 
 sub ansi24bbg { goto &rgb_to_ansi24b_bg_code }
 
+
+my $cd_cache;
+sub detect_color_depth {
+    my $cd;
+    if ($ENV{KONSOLE_DBUS_SERVICE} || $ENV{KONSOLE_DBUS_SESSION}) {
+        # assume konsole is recent version and support 24bit color
+        $cd = 2**24;
+    } elsif (($ENV{TERM} // "") =~ /256color/) {
+        $cd = 256;
+    } else {
+        $cd = 16;
+    }
+    $cd_cache = $cd;
+    $cd;
+}
+
+sub rgb_to_ansi_fg_code {
+    my ($rgb) = @_;
+    my $cd = $ENV{COLOR_DEPTH};
+    unless (defined $cd) {
+        detect_color_depth() unless defined $cd_cache;
+        $cd = $cd_cache;
+    }
+    if ($cd >= 2**24) {
+        rgb_to_ansi24b_fg_code($rgb);
+    } elsif ($cd >= 256) {
+        rgb_to_ansi256_fg_code($rgb);
+    } else {
+        rgb_to_ansi16_fg_code($rgb);
+    }
+}
+
+sub ansifg { goto &rgb_to_ansi_fg_code }
+
+sub rgb_to_ansi_bg_code {
+    my ($rgb) = @_;
+    my $cd = $ENV{COLOR_DEPTH};
+    unless (defined $cd) {
+        detect_color_depth() unless defined $cd_cache;
+        $cd = $cd_cache;
+    }
+    if ($cd >= 2**24) {
+        rgb_to_ansi24b_bg_code($rgb);
+    } elsif ($cd >= 256) {
+        rgb_to_ansi256_bg_code($rgb);
+    } else {
+        rgb_to_ansi16_bg_code($rgb);
+    }
+}
+
+sub ansibg { goto &rgb_to_ansi_bg_code }
+
 1;
 # ABSTRACT: Routines for dealing with ANSI colors
 
@@ -254,7 +313,7 @@ Color::ANSI::Util - Routines for dealing with ANSI colors
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
@@ -384,11 +443,43 @@ Yakuake.
 
 Alias for rgb_to_ansi24b_bg_code().
 
+=head2 rgb_to_ansi_fg_code($rgb) => STR
+
+Return ANSI escape code to set 24bit/256/16 foreground color (which color depth
+used is determined by C<COLOR_DEPTH> environment setting or the latest
+detect_color_depth() result). In other words, this function automatically
+chooses rgb_to_ansi{24b,256,16}_fg_code().
+
+=head2 ansifg($rgb) => STR
+
+Alias for rgb_to_ansi_fg_code().
+
+=head2 rgb_to_ansi_bg_code($rgb) => STR
+
+Return ANSI escape code to set 24bit/256/16 background color (which color depth
+used is determined by C<COLOR_DEPTH> environment setting or the latest
+detect_color_depth() result). In other words, this function automatically
+chooses rgb_to_ansi{24b,256,16}_bg_code().
+
+=head2 ansibg($rgb) => STR
+
+Alias for rgb_to_ansi_bg_code().
+
+=head2 detect_color_depth() => INT
+
+Detect color depth.
+
+=head1 ENVIRONMENT
+
 =head1 BUGS/NOTES
 
 Algorithm for finding closest indexed color from RGB color currently not very
 efficient. Probably can add some threshold square distance, below which we can
 shortcut to the final answer.
+
+=head1 TODO
+
+Routine to convert ANSI escape code, e.g. C<\e[31;1m> into RGB value (ff0000).
 
 =head1 SEE ALSO
 
